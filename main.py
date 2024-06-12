31966c024b4c89150f4719ee77f73e9adbc76117
sultan7557 <m.sultan2722@gmail.com>
2024-06-12 20:39:06 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+from logo import logo
+
+print(logo)
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What is your required drink?({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+    
+
+        
